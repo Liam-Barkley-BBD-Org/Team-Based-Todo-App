386714fa5a883cb7bf074e1d9bd2d97ed6701e73
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-06-02 17:48:48 +0200

    s3 deployment

--- terraform/front-end.tf
+++ terraform/front-end.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket_block" {
-  bucket = aws_s3_bucket.private_bucket.id
+  bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
