f50fdfedb241fa621713a59fa749d47bae8d9ace
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-06-02 17:46:00 +0200

    s3 deployment

---
+++ terraform/front-end.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    Environment = "Prod"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_block" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
